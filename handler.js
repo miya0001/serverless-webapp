e989614dba8e812cf08f3d37ce94fe78dac7a0e8
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-12-16 20:45:59 +0900

    integrate lambda-proxy

--- handler.js
+++ handler.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports.html = (event, context, callback) => {
+module.exports.app = (event, context, callback) => {
   callback(null, { 
     statusCode: 200, 
     body: `<html><head><title>Hello World</title></head><body><h1>Hello World!</h1><pre style="background-color: #f5f5f5; padding: 10px; border: 1px solid #dedede;">${JSON.stringify(event, null, 2)}</pre></body></html>`, 
