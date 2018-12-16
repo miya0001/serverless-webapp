5ee4344707307e609be15c9ba8a5c52b8d3970b8
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-12-17 01:59:52 +0900

    add handler for routing

--- handler.js
+++ handler.js
@@ -1,12 +1,26 @@
 'use strict';
 
+const yaml = require('js-yaml')
+const fs = require('fs')
+const path = require('path')
+
+const map = yaml.safeLoad(fs.readFileSync('./site.yml', 'utf8'))
+
+const getHandler = (string) => {
+  const handler = require(path.join(__dirname, string.split(/\./)[0]))
+  return handler[string.split(/\./)[1]]
+}
+
 module.exports.app = (event, context, callback) => {
-  callback(null, { 
-    statusCode: 200, 
-    body: `<html><head><title>Hello World</title></head><body><h1>Hello World!</h1><pre style="background-color: #f5f5f5; padding: 10px; border: 1px solid #dedede;">${JSON.stringify(event, null, 2)}</pre></body></html>`, 
-    headers: { 
-        "Content-Type": "text/html" 
-      } 
+  let handler = getHandler(map.defaultHandler)
+  let params = {}
+
+  for (const key in map.sitemap) {
+    if (event.path.match(new RegExp(`^${key}$`))) {
+      params = map.sitemap[key]
+      handler = getHandler(map.sitemap[key].handler)
     }
-  );
+  }
+
+  callback(null, handler(params, event, context))
 }
